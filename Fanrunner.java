4732f75b97275ab99b4d4532996697405ac4af35
BishalTiwarii <129974785+BishalTiwarii@users.noreply.github.com>
2023-04-06 19:30:39 +0530

    Update Fanrunner.java

--- Fanrunner.java
+++ Fanrunner.java
@@ -3,13 +3,18 @@ package bishal;
 public class Fanrunner {
 
 	public static void main(String[] args) {
-		Fan fan=new Fan("Usha", 22.05, 3, true);
-		System.out.println(fan);
+	Fan fan=new Fan("Usha", 22.05, 3, true);
+	System.out.println(fan);
         fan.setSpeed(5);
         System.out.println(fan);
         fan.setRadius(20.00);
         fan.setMake("Havels");
         System.out.println(fan);
+        fan.switchoff();
+        System.out.println(fan);
+        fan.switchOn();
+        System.out.println(fan);
+
 	}
 
 }
