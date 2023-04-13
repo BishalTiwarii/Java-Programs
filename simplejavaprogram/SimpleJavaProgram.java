3b11ca97814c33f3eb07f2ffc3ddfff59c472600
BishalTiwarii <129974785+BishalTiwarii@users.noreply.github.com>
2023-04-13 14:05:27 +0530

    Add files via upload

---
+++ simplejavaprogram/SimpleJavaProgram.java
@@ -0,0 +1,22 @@
+package simplejavaprogram;
+
+public class SimpleJavaProgram {
+
+	public static class HelloWorld {
+		public static void sayHelloWorldThrice() 
+		{
+			System.out.println("hello world");
+			System.out.println("hello world");
+			System.out.println("hello world");
+		}
+
+		public static void main(String[] args) 
+		{
+			sayHelloWorldThrice();
+
+		}
+
+	
+	}}
+
+
